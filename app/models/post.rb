8c54c4cf5b857836b847ffb782872c5b02a791dc
Sean Oughton <seanoughton@gmail.com>
2018-05-01 23:12:17 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :comments
 
+  def comment_content
+    binding.pry
+  end
 end
