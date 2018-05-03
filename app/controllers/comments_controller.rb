fd21712cc327c0af1205dcbd3a9053e2446e2b8c
Sean Oughton <seanoughton@gmail.com>
2018-05-03 21:42:33 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,12 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
     @comment.user_attributes=(comment_params)
+=begin
     if !params[:comment][:user][:attributes][:username].empty?
       @user = User.create(username: params[:comment][:user][:attributes][:username])
       @user.comments << @comment
     end
+=end
     @post = Post.find(params[:post_id])
     @post.comments << @comment
     @post.save
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,12 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   def user_attributes=(attributes)
-    self.user = User.find(attributes[:user_id])
+    binding.pry
+    if attributes.include?(:user_name)
+      @user = User.create(username: attributes[:username])
+      @user.comments << @comment
+    end
+    #self.user = User.find(attributes[:user_id])
   end
   #accepts_nested_attributes_for :post
   #accepts_nested_attributes_for :user
