24f8360727db6a1f00e0de5c3532735e02d40f6c
Sean Oughton <seanoughton@gmail.com>
2018-05-03 23:35:34 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,6 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.create(comment_params)
-    #@post = Post.find(params[:post_id])
-    #@post.comments << @comment
-    #@post.save
-    #redirect_to @post
     redirect_to @comment.post
   end
 
