1575d002c53d8202dc1ab2ac75200bbcac29ba0a
Sean Oughton <seanoughton@gmail.com>
2018-05-03 22:15:27 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   def user_attributes=(attributes)
+    binding.pry
     if attributes.include?(:username)
       user = User.find_or_create_by(username: attributes[:username])
       user.comments << self
@@ -10,6 +11,10 @@ class Comment < ActiveRecord::Base
       user = User.find(attributes[:user_id])
       user.comments << self
     end
+  end
+
+  def user_attributes
+    self.user_attributes
   end
 
 end
