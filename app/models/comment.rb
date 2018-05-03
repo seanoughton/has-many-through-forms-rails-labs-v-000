f6eccadb51217b639355c5508af3a4a817828a89
Sean Oughton <seanoughton@gmail.com>
2018-05-03 22:57:53 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,16 +2,19 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  def user_attributes=(attributes)
+  accepts_nested_attributes_for :users
+=begin
+  def user_attributes=(user_attributes)
     binding.pry
-    if attributes.include?(:username)
+    if user_attributes.include?(:username)
       user = User.find_or_create_by(username: attributes[:username])
       user.comments << self
-    else
-      user = User.find(attributes[:user_id])
-      user.comments << self
+    #else
+      #user = User.find(attributes[:user_id])
+      #user.comments << self
     end
   end
+=end
 
   def user_attributes
     self.user_attributes
