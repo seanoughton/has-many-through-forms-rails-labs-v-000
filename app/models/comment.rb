abb46fe793e3e26f526bc1ed0d127a13b0bcb923
Sean Oughton <seanoughton@gmail.com>
2018-05-03 22:37:04 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,15 +5,15 @@ class Comment < ActiveRecord::Base
   def user_attributes=(attributes)
     if attributes.include?(:username)
       user = User.find_or_create_by(username: attributes[:username])
-      self.user = user
-      #self.user.comments << self
-      self.user.save
+      #self.user = user
+      binding.pry
+      user.comments << self
+      #self.save
     else
       user = User.find(attributes[:user_id])
       self.user = user
-      self.user.save
+      self.save
     end
-    user
   end
 
   def user_attributes
